18f93b65c650bcd660e804a9dcf2b776948c9d2a
Dong-Young Kim <31337.persona@gmail.com>
2021-12-15 19:21:14 +0900

    fix: enable `esModuleInterop`

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,5 @@
 import chalk from "chalk";
-import * as inquirer from "inquirer";
+import inquirer from "inquirer";
 import ora from "ora";
 
 import { createLineStream, validateEmail } from "./utils.js";
