c741111c08ca88f88edc1efe4db90a82d5f52d08
Dong-Young Kim <31337.persona@gmail.com>
2022-07-05 02:53:01 +0900

    refactor: refactor cli module

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,16 +1,39 @@
 import { Command } from "commander";
+import inquirer from "inquirer";
+import { checkFileExist, readPackage } from "../helper.js";
+import questions from "../questions.js";
+import { limit, org } from "./commands/index.js";
 
-import {
-  organizationInvitationCommand,
-  rateLimitCommand,
-  repositoryInvitationCommand,
-} from "./commands.js";
+const { name, version } = readPackage();
 
-const program = new Command();
+const cli = new Command();
 
-program
-  .addCommand(organizationInvitationCommand(), { isDefault: true })
-  .addCommand(repositoryInvitationCommand())
-  .addCommand(rateLimitCommand());
+cli
+  .name(name)
+  .version(version)
+  .description("GitHub organization invitation CLI")
+  .showHelpAfterError()
+  .showSuggestionAfterError();
 
-export default program;
+cli
+  .option(
+    "--token <token>",
+    "GitHub personal access token",
+    process.env.GITHUB_TOKEN,
+  )
+  .option(
+    "-f, --file <filename>",
+    "specify file containing invitees",
+    checkFileExist,
+  )
+  .hook("preAction", async (command) => {
+    let { token } = command.optsWithGlobals();
+    if (!token) {
+      ({ token } = await inquirer.prompt(questions.token(token)));
+      command.setOptionValue("token", token);
+    }
+  });
+
+cli.addCommand(org, { isDefault: true }).addCommand(limit);
+
+export default cli;
